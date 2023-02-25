8da9009fe636171f5568e4862d2ea77dc723c60b
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-02-25 19:14:50 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `job-board`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-sitemap"]
+};
