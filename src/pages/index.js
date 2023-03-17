8deab235486d0a3ef8fb121b5a69a433a6af9254
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-03-17 23:07:08 +0530

    Getting description into display

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,18 @@ import {
   Box,
   Tag,
   Image,
-  Button
+  Button,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react'
 import LocationLogo from '../components/LocationLogo';
 import SvgCompanyLogo from '../components/CompanyLogo';
 import SvgDollarSignSvgrepoCom from '../components/DollarLogo';
 import { SEO } from '../components/seo';
+import parse from 'html-react-parser';
 import aiJobs from '../../content/ai-jobs.json'
 import dataJobs from '../../content/datajobs_jobs.json'
 import machinehack from '../../content/machinehack_jobs.json'
@@ -51,6 +57,7 @@ const IndexPage = () => {
     }
     try {
       fetchData()
+      console.log('fetched')
     } catch (error) {
       console.log(error)
     }
@@ -66,42 +73,57 @@ const IndexPage = () => {
             // setNumberOfPages(Math.ceil(dataObject.length / 25))
             // console.log(job)
             return (
-              <LinkBox variant='' key={index} href={job.apply_link}>
 
-                {/* Stack of 2 elements: Image and Box */}
-                <Stack bg={'white'} direction={['column', 'row']} w={['sm', null, '3xl']} spacing={'-4'} borderRadius='md' shadow={'base'} >
-                  {job.company_logo ? (<Image
-                    borderRadius='full'
-                    // boxSize='10'
-                    verticalAlign={'middle'}
-                    src={job.company_logo}
-                    alt='Organisation Logo'
-                  />) : (<Circle size={['12', '16']} m={['3', '5']} bg='gray.50' ><SvgCompanyLogo /></Circle>)}
+              <Accordion allowToggle>
+                <AccordionItem>
+                  {/* <LinkBox variant='' key={index} href={job.apply_link}> */}
+                  <h3>
+                    <AccordionButton p='-4'>
+                      {/* Stack of 2 elements: Image and Box */}
+                      <Stack bg={'white'} direction={['column', 'row']} w={['sm', null, '3xl']} spacing={'-4'} borderRadius='md' shadow={'base'} >
+                        {job.company_logo ? (<Image
+                          borderRadius='full'
+                          // boxSize='10'
+                          verticalAlign={'middle'}
+                          src={job.company_logo}
+                          alt='Organisation Logo'
+                        />) : (<Circle size={['12', '16']} m={['3', '5']} bg='gray.50' ><SvgCompanyLogo /></Circle>)}
 
-                  {/* Details of the job */}
-                  <Box w='inherit' p={['3', '4', '5']}>
+                        {/* Details of the job */}
+                        <Box w='inherit' p={['3', '4', '5']}>
 
-                    {/* Title and type tag */}
-                    <LinkOverlay href={job.apply_link} isExternal>
-                      <Flex>
-                        <Text fontSize={['lg', 'xl']}>{job.title}</Text>
-                        <Spacer />
-                        {job.type !== 'Not mentioned' ? (<Tag size={['sm', 'md']} fontWeight={'extrabold'}>{job.type}</Tag>) : null}
-                      </Flex>
-                    </LinkOverlay>
+                          {/* Title and type tag */}
+                          {/* <LinkOverlay href={job.apply_link} isExternal> */}
+                          <Flex>
+                            <Text fontSize={['lg', 'xl']}>{job.title}</Text>
+                            <Spacer />
+                            {job.type !== 'Not mentioned' ? (<Tag size={['sm', 'md']} fontWeight={'extrabold'}>{job?.type}</Tag>) : null}
+                          </Flex>
+                          {/* </LinkOverlay> */}
 
-                    {/* Location with logo */}
-                    <Stack direction={'row'} spacing={2}><LocationLogo /><Text fontSize={['md', 'lg']}>{job.location}</Text></Stack>
+                          {/* Location with logo */}
+                          {job.location && <Stack direction={'row'} spacing={2}><LocationLogo /><Text fontSize={['md', 'lg']}>{job.location}</Text></Stack>}
 
-                    {/* Salary */}
-                    {job.salary_end ? (<Stack direction={'row'}><SvgDollarSignSvgrepoCom /><Text fontSize={['md', 'lg']}>{job.salary_start} - {job.salary_end}K</Text></Stack>) : null}
+                          {/* Salary */}
+                          {job.salary_end ? (<Stack direction={'row'}><SvgDollarSignSvgrepoCom /><Text fontSize={['md', 'lg']}>{job.salary_start} - {job.salary_end}K</Text></Stack>) : null}
 
-                    {/* Tags */}
-                    {job.tags ? (job.tags.map((tag, index) => (<Tag key={index} size={['xs', 'sm', 'md', 'lg']} fontSize={['xs', 'sm', 'md']} p={['1', '2']} m={'1'} variant={'outline'}>{tag}</Tag>))) : null}
+                          {/* Tags */}
+                          {job.tags ? (job.tags.map((tag, index) => (<Tag key={index} size={['xs', 'sm', 'md', 'lg']} fontSize={['xs', 'sm', 'md']} p={['1', '2']} m={'1'} variant={'outline'}>{tag}</Tag>))) : null}
 
-                  </Box>
-                </Stack>
-              </LinkBox>
+                        </Box>
+                      </Stack>
+                      {/* <AccordionIcon /> */}
+                    </AccordionButton>
+                  </h3>
+                  <AccordionPanel w={['sm', null, '3xl']} bgColor='gray.200' pl={['3', '4', '5']} pb={4}>
+                    {/* {job?.description && <Text fontSize={['md', 'lg']}>{JSON.parse(job.description)}</Text>} */}
+                    
+                    {parse(job.description)}
+                  </AccordionPanel>
+                  {/* </LinkBox> */}
+
+                </AccordionItem>
+              </Accordion>
             )
           })}
           <Stack p='5' justify={'center'} direction={'row'}>
@@ -112,7 +134,7 @@ const IndexPage = () => {
         </VStack>
       </Wrap>
 
-    </main>
+    </main >
   )
 }
 
